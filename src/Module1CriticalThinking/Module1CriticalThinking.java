4f41687a8ffdc81351d56c2cc9d7e33101fc0f7f
Michael Mooney <michael.mooney1@csuglobal.edu>
2025-06-15 19:24:41 -0600

    Personal Information project for CSC320-1 Module 1 Critical Thinking

---
+++ src/Module1CriticalThinking/Module1CriticalThinking.java
@@ -0,0 +1,38 @@
+package Module1CriticalThinking;
+
+import java.util.Scanner;
+
+public class Module1CriticalThinking {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		String firstName;
+		String lastName;
+		String streetAddress;
+		String city;
+		String state;
+		String zipcode;
+		
+		System.out.println("Enter your first name:");
+		firstName = scnr.nextLine();
+		
+		System.out.println("Enter your last name:");
+		lastName = scnr.nextLine();
+		
+		System.out.println("Enter your street address:");
+		streetAddress = scnr.nextLine();
+		
+		System.out.println("Enter your city:");
+		city = scnr.nextLine();
+		
+		System.out.println("Enter your state:");
+		state = scnr.nextLine();
+		
+		System.out.println("Enter your zipcode");
+		zipcode = scnr.nextLine();
+		
+		System.out.println(firstName + " " + lastName);
+		System.out.println(streetAddress);
+		System.out.println(city + ", " + state + " " + zipcode);
+	}
+}
